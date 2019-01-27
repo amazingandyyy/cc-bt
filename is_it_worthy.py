0e8c958d2572bd0b260904d402dee1375eb084f2
Andy Chen <amazingandyyy@gmail.com>
2019-01-26 21:08:57 -0800

    update

--- is_it_worthy.py
+++ is_it_worthy.py
@@ -1,14 +1,17 @@
 import math
 
-amount = float(input("How big is your debt? "))
-transfer_fee = float(input("What % is the transfer fee? "))/100*amount
+debt_size = float(input("How big is your debt? "))
 apr = float(input("What is the APR for the debt? "))/100
+transfer_fee = (float(input("What % is the transfer fee? "))/100)*debt_size
 daily_rate = apr/365
 days = 1
-accumulate_interest = (amount*math.pow(1+daily_rate, days)-amount)
+accumulate_interest = (debt_size*math.pow(1+daily_rate, days)-debt_size)
 while transfer_fee > accumulate_interest:
  days = days + 1
- accumulate_interest = (amount*math.pow(1+daily_rate, days)-amount)
+ accumulate_interest = (debt_size*math.pow(1+daily_rate, days)-debt_size)
 
-print("DAYS" + str(days))
-print("If you cannot pay $" + str(amount) + " in " + str(days) + " days, it's worthy to do this balance transfer!")
+answer = input("Can you pay $"+ str(debt_size) + " in " + str(days) + "days?[y/n]")
+if answer == "n":
+ print("OKAY: It may be worthy to transfer $"+ str(debt_size) + "right now!")
+elif answer == "y":
+ print("Well, it is NOT worthy to transfer!")
